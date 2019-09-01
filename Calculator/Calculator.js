1b69528c8e176ae9995bc7a918e3ad830614a87f
Brian Figueroa <brianfigueroa001@gmail.com>
2019-08-31 22:16:24 -0400

    New UI and allow calc with negative num's ToDo: decimals

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -3,63 +3,102 @@ class Calculator
 	//Evaluate an expression that's in postfix notation.
 	evaluate(postExpr)
 	{
-		//The expression
+		//Store each operand and operator seperately by splitting the expression up by spacings.
 		let exprArr = postExpr.split(" ");
 		//Used to enforce order of operations
 		let stack = [];
 		//Tracks exprArr index
 		let i = 0;
-
+		
+		/*
+		The for loop visits every operand and operator in the expression.
+		
+		All operands are pushed into the stack.
+		
+		When an operator is found, it will pop operands from the stack and perform the appropriate calculation.
+		This calculation is one of two cases:
+		
+				CASE 1. A normal arithmetic operation of the first and second operand, both operands being the top two elements in the stack.
+						EX: (2 + 3). 
+						
+						y = 3, and x = 3.
+							
+						In this case, two elements are popped from the stack. 
+						The first popped element is the second operand and the second popped element is the first operand.
+						The visited operator will determine the appropriate calculation to perform between these operands.
+								
+				CASE 2. The MULTIPLICATION of a -1 in front of an expression enclosed in parenthesis. The stack will only have 
+						EX: -(2 + 3) => -(5). 
+						
+						y = 5, and x = -1.
+						
+						The visited operator will be '-' which is actually representing the negative in front of an enclosed expression, not a substraction.
+						
+						Following the example, (2 + 3) was calculated in the PREVIOUS iteration of the for loop, and the result (5) was pushed into the stack.
+						In the CURRENT iteration, the top element of the stack is the result (5) which gets popped and stored in variable y.
+						The stack will be empty at this point, indicating that this is CASE 2. Variable x will be assigned -1 to represent the negative 
+						in front of the enclosed expression. x and y will be multiplied.
+						
+				In both cases, the calculated result will be pushed into the stack.
+			
+		Once the entire array has been visited, the last element in the stack is the final result.
+		*/
 		for (i = 0; i < exprArr.length; i++)
-		{
-				//skip spaces
-				if (exprArr[i] === ' ')
-				{
-					continue;
-				}
-				//Gets the substring from the splitted expression
-				let substring = exprArr[i];
-				//If the scanned substring is an operand
+		{	
+			//Store the operand OR operator visited in this iteration.
+			let substring = exprArr[i];
 
+			//Push all operands into the stack.
+			if(substring !== '+' && substring !== '-' && substring !== '*' && substring !== '/')
+			{
+				stack.push(substring);
+			}
+			else //Perform the appropriate calculation with the  given operator
+			{	
+				//Stores the second operand OR the result of a calculated expression enclosed in parenthesis
+				let y;
+				//Stores the first operand OR a -1 that was in front of an expression enclosed in parenthesis.
+				let x;
+				//Stores the result after the calculation is performed.
+				let result;
+
+				y = parseInt(stack.pop());
 				
-				if(substring !== '+' && substring !== '-' && substring !== '*' && substring !== '/')
+				if (stack.length === 0) //CASE 2
 				{
-					stack.push(substring);
+					x = -1;
+					//Operator is '-' and it actually represents a negative 1. Change the operator to multiplication.
+					substring = '*'; 
 				}
-				else //If the scanned substring is an operator
-				{				
-					//Second operand
-					let y = parseInt(stack.pop());
-					//First operand
-					let x = parseInt(stack.pop());
+				else //CASE 1
+				{
+					x = parseInt(stack.pop());
+				}
 					
-					//Perform operation and get the integer value
-					let result;
-					
-					if (substring === '+')
-					{
-						result = x + y;
-					}
-					else if (substring === '-')
-					{
-						result = x - y;
-					}
-					else if (substring === '*')
-					{
-						result = x * y;
-					}
-					else // '/'
-					{
-						result = x / y;
-					}		
+				if (substring === '+')
+				{
+					result = x + y;
+				}
+				else if (substring === '-')
+				{
+					result = x - y;
+				}
+				else if (substring === '*')
+				{
+					result = x * y;
+				}
+				else
+				{
+					result = x / y;
+				}	
 
-					//Convert the result into a string to push into the stack.
-					let stringResult = result.toString();
-					stack.push(stringResult);
-				}
+				//Convert the result into a string and push it into the stack.
+				let stringResult = result.toString();
+				stack.push(stringResult);				
+			}	
 		}
 		
-		//pop the result (which should be the only element in the stack by now) and output it.
+		//The last element in the stack is the total evaluated result of the epression.
 		return stack.pop();
 	}
 
@@ -67,15 +106,10 @@ class Calculator
 
 class InfixToPostfix
 {	
-	constructor()
-	{
-		//Used to store parenthesis and operators
-		this.stack = [];
-	}
-	
 	//Converts an expression from infix notation to postfix.
 	convertToPostFix(inExpr)
 	{
+		let stack = [];
 		//Array containing the substrings of the expression excluding spaces.
 		let exprArr = inExpr;
 		//Store the expression in postfix notation.
@@ -94,8 +128,8 @@ class InfixToPostfix
 			{
 				continue;
 			}
-			//If it is a digit
-			else if (isNaN(exprChar) === false)
+			//If exprChar is a digit OR  a '-' representing a neg. number (there shouldn't be a space after the '-' for neg. numbers)
+			else if (!isNaN(exprChar) || (exprChar == '-' && i + 1 < exprArr.length && !isNaN(exprArr[i + 1]) && exprArr[i + 1] !== ' '))
 			{
 				//Append the digit to the string.
 				postExpr += exprChar;
@@ -103,34 +137,32 @@ class InfixToPostfix
 				//Append adjacent digits until a space is found.		
 				while (i + 1 < exprArr.length && !isNaN(exprArr[i + 1]) && exprArr[i + 1] !== ' ')
 				{
-					i++;
-					exprChar = exprArr[i];
+					exprChar = exprArr[++i];
 					postExpr += exprChar;
 				}
 				
 				//Append a space after the operand is inserted
 				postExpr += ' ';
-					
 			}
-			//If the scanned string is a parenthesis
+			//If the scanned string is '(' OR ')' OR a '-' representing a neg. number in front of an opening parenthesis (ex: -(2 + 3) or -(-2 + 3)
 			else if (exprChar === '(' || exprChar === ')')
 			{	
 				//Push all left paranthesis into the stack
 				if (exprChar === '(') 
 				{
-					this.stack.push(exprChar);
+					stack.push(exprChar);
 				}
 				//exprChar == ')'. Pop all elements from the stack until a '(' is found
 				else
 				{
 					//Append everything from the stack before a '(' is found.
-					while (this.stack.length > 0 && this.stack[this.stack.length - 1] !== '(') 
+					while (stack.length > 0 && stack[stack.length - 1] !== '(') 
 					{
-						postExpr += this.stack.pop() + ' ';
+						postExpr += stack.pop() + ' ';
 					}
 						
 					//Discard the '('
-					this.stack.pop();
+					stack.pop();
 				}
 			}
 			//If the scanned string is an operator
@@ -143,11 +175,11 @@ class InfixToPostfix
 				 - If the scanned operator's precedence is greater than
 				 the operator that's on top of the stack.
 				 */
-				if (this.stack.length === 0 || this.stack[this.stack.length - 1] === '('
-					|| precedence(exprChar) > precedence(this.stack[this.stack.length - 1])) 
+				if (stack.length === 0 || stack[stack.length - 1] === '('
+					|| this.precedence(exprChar) > this.precedence(stack[stack.length - 1])) 
 				{
 					//Push it into the stack
-					this.stack.push(exprChar);
+					stack.push(exprChar);
 				} 
 				/*
 				Pop and append all operators from the stack that have
@@ -158,54 +190,49 @@ class InfixToPostfix
 				*/
 				else 
 				{
-					while (this.stack.length > 0 && this.stack[this.stack.length - 1] !== '('
-						  && precedence(exprChar) <= precedence(this.stack[this.stack.length - 1])) 
+					while (stack.length > 0 && stack[stack.length - 1] !== '('
+						  && this.precedence(exprChar) <= this.precedence(stack[stack.length - 1])) 
 					{
-						postExpr += this.stack.pop() + ' ';
+						postExpr += stack.pop() + ' ';
 					}
 
 						//Push this character into the stack.
-						this.stack.push(exprChar);
+						stack.push(exprChar);
 				}
 			}
 			//Invalid character found
 			else
 			{
-				return 'Invalid_expression.';
+				return 'Invalid Expression.';
 			}
 		}
 					
-		while (this.stack.length > 0)
+		//Pop the rest of the stack and append them into the string
+		while (stack.length > 0)
 		{
-			if (this.stack.length - 1 > 0)
-			{
-				postExpr += this.stack.pop() + ' ';
-			}
-			else
-			{
-				postExpr += this.stack.pop();
-			}
+			postExpr += stack.pop() + ' ';
 		}
 	
+		//Remove the space at the end of the string.
+		postExpr = postExpr.substring(0, postExpr.length - 1);
+		
 		return postExpr;
 	 }
 		
+	//Utility function for method convertToPostFix. Return precedence of the operator
+	precedence(exprChar)
+	{
+		if (exprChar === '+' || exprChar === '-')
+		{
+			return 1;
+		}
+		else 
+		{
+			return 2;
+		}
+	}
 
 }
-
-//Utility function for method convertToPostFix. Return precedence of the operator
-function precedence(exprChar)
-{
-	if (exprChar === '+' || exprChar === '-')
-	{
-		return 1;
-	}
-	else 
-	{
-		return 2;
-	}
-}
-
 
 function handleKeyPress(event)
 {
@@ -216,6 +243,7 @@ function handleKeyPress(event)
 	let converter = new InfixToPostfix();
 
 	let postfixInput = converter.convertToPostFix(infixInput);
+	alert(postfixInput);
 	let result = calculator.evaluate(postfixInput);
 	
 	document.getElementById("userOutput").innerHTML = result;
